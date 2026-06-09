bfd0ece2498352fa91cd2732da54f05b307ca771
ap <anpatidar@bestpeers.com>
2026-06-09 17:52:26 +0530

    update index.js

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! deployment: 9 ');
+  console.log('Received request at /');
+  res.send('Hello World! deployment: 4');
 });
 
 app.get('/exit', (req, res) => {
+  console.log('Received request at /exit');
   res.send('Server is shutting down...');
   process.exit(0);
 });
